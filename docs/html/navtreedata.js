538b87d67c4a387188e757181e60499c04b1c715
Rhys <echokillz123@outlook.com>
2025-08-11 13:12:19 +0100

    Fixed Documentation

--- docs/html/navtreedata.js
+++ docs/html/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "Conference App", "index.html", [
+  [ "Starter App", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
